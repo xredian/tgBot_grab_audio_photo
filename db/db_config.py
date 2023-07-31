96f54e0d78dde72538e4ce12b124f2aefe930efe
xredian <ulyanadyakova@gmail.com>
2023-07-31 16:16:15 +0200

    setting db

---
+++ db/db_config.py
@@ -0,0 +1,26 @@
+from logger import log_msg
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy import Integer, Table, Column
+from sqlalchemy.dialects.postgresql import VARCHAR
+
+
+def setup_db(env):
+    """
+    Setting up DB
+
+    :return: database, questions, additional_questions, users
+    """
+    base_url = env.db_engine
+    metadata = MetaData()
+
+    audio_messages = Table(
+        'audio_messages',
+        metadata,
+        Column('uid', Integer, primary_key=True),
+        Column('audio_message', VARCHAR(255)),
+    )
+
+    engine = create_engine(base_url)
+    metadata.create_all(engine)
+    log_msg('success', 'Tables created in DB')
+    return engine, audio_messages
